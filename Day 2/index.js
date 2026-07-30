bf39d27e9d38bcdf4806799560890d657f71141c
simransssCsE-27 <simranyadav20180093@gmail.com>
2026-07-30 11:17:19 +0530

    day2

---
+++ Day 2/index.js
@@ -0,0 +1,7 @@
+let num = 125.7667;
+
+console.log(num.toFixed(2));      // 125.77
+console.log(num.toPrecision(5));  // 125.77
+console.log(Number.isInteger(num));  //false
+console.log(parseInt(num)); // 125
+ 
---
+++ Day 2/object.js
@@ -0,0 +1,11 @@
+//mathobject
+console.log(Math.PI); 
+console.log(Math.sqrt(16));
+console.log(Math.pow(2, 3));
+console.log(Math.max(10, 20, 30));
+console.log(Math.min(10, 20, 30));
+
+console.log(Math.floor(4.8));
+console.log(Math.round(10.01));
+console.log(Math.ceil(10.01));
+console.log(Math.floor(Math.random()*100)+1);
